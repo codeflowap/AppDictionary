77e1081c74ba882049b7251abba51c5026d3fa96
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-21 14:49:37 -0500

    Taken care of false entries

--- appdic.py
+++ appdic.py
@@ -3,7 +3,10 @@ import json
 entries = json.load(open("entries.json"))
 
 def findEntry(word):
-    return entries[word]
+    if word in entries:
+        return entries[word]
+    else:
+        return "The word does not exist"
 
 word = input("Enter a word: ")
 print (findEntry(word))
