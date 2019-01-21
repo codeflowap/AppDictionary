210509a7917ef74d4de85c4a7c010055e2d14346
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-21 15:07:28 -0500

    Added word suggestions

--- appdic.py
+++ appdic.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 entries = json.load(open("entries.json"))
 
@@ -6,6 +7,12 @@ def findEntry(word):
     word = word.lower()
     if word in entries:
         return entries[word]
+    elif len(get_close_matches(word, entries.keys()))>0:
+        ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
+        if ans == "Y" or "y":
+            return entries[get_close_matches(word, entries.keys())[0]]
+        else:
+            return "Your entry doesn't exist."
     else:
         return "The word does not exist"
 
