104f66a1745b8b236670e9f87b5531adb9276aac
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-22 21:23:19 -0500

    Fxed bug for city and special names

--- appdic.py
+++ appdic.py
@@ -5,16 +5,25 @@ entries = json.load(open("entries.json"))
 
 def findEntry(word):
     word = word.lower()
-    if word in entries:
+    if word.upper()[0]+word[1:] in entries:
         return entries[word]
     elif len(get_close_matches(word, entries.keys()))>0:
-        ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
-        if ans == "Y" or "y":
-            return entries[get_close_matches(word, entries.keys())[0]]
+            ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
+            if ans == "Y" or "y":
+                return entries[get_close_matches(word, entries.keys())[0]]
+            else:
+                return "Your entry doesn't exist."
+    else:    
+        if word in entries:
+            return entries[word]
+        elif len(get_close_matches(word, entries.keys()))>0:
+            ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
+            if ans == "Y" or "y":
+                return entries[get_close_matches(word, entries.keys())[0]]
+            else:
+                return "Your entry doesn't exist."
         else:
-            return "Your entry doesn't exist."
-    else:
-        return "The word does not exist"
+            return "The word does not exist"
 
 word = input("Enter a word: ")
 print (findEntry(word))
