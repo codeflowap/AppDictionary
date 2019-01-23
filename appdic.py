a9fd9e0ea5b0a813749c02ce5fdfec800dd18086
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-22 21:45:18 -0500

    Make entry ready to be looked up

--- appdic.py
+++ appdic.py
@@ -4,27 +4,26 @@ from difflib import get_close_matches
 entries = json.load(open("entries.json"))
 
 def findEntry(word):
-    word = word.lower()
-    if word.upper()[0]+word[1:] in entries:
+    word = correctEntry(word)  
+    if word in entries:
         return entries[word]
     elif len(get_close_matches(word, entries.keys()))>0:
-            ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
-            if ans == "Y" or "y":
-                return entries[get_close_matches(word, entries.keys())[0]]
-            else:
-                return "Your entry doesn't exist."
-    else:    
-        if word in entries:
-            return entries[word]
-        elif len(get_close_matches(word, entries.keys()))>0:
-            ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
-            if ans == "Y" or "y":
-                return entries[get_close_matches(word, entries.keys())[0]]
-            else:
-                return "Your entry doesn't exist."
+        ans = input("Did you mean %s instead? [Y/N]" % get_close_matches(word, entries.keys())[0])
+        if ans == "Y" or "y":
+            return entries[get_close_matches(word, entries.keys())[0]]
         else:
-            return "The word does not exist"
+            return "Your entry doesn't exist."
+    else:
+        return "The word does not exist"
+
+def correctEntry(word):
+    """Keep the first letter as it is and lowercase the rest"""
+    firstletter = word[0]
+    wordlower = word[1:].lower()
+    correctOriginalWord = firstletter + wordlower
+    return correctOriginalWord
 
 word = input("Enter a word: ")
 print (findEntry(word))
+
 
