e4f6105864853583c03ac2fb75f69925a02e94eb
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-01-22 23:02:09 -0500

    Created method for double entry check

--- appdic.py
+++ appdic.py
@@ -4,7 +4,7 @@ from difflib import get_close_matches
 entries = json.load(open("entries.json"))
 
 def findEntry(wordEntered):
-    word = correctEntry(wordEntered)  
+    word = correctEntry(wordEntered)
     if word in entries:
         return entries[word]
     elif word[0].upper()+word[1:] in entries:
@@ -20,13 +20,32 @@ def findEntry(wordEntered):
         return "The word does not exist"
 
 def correctEntry(word):
-    """Keep the first letter as it is and lowercase the rest"""
+    """Keep the first letter as it is and make the rest lowercase"""
     firstletter = word[0]
     wordlower = word[1:].lower()
     correctOriginalWord = firstletter + wordlower
     return correctOriginalWord
 
+def checkForDouble(word):
+    """ Checks if a word with both uppercase AND lowercase first letter has entries"""
+    word = correctEntry(word)
+    wordFirstUpper = word[0].upper()+word[1:]
+    wordFirstLower = word[0].lower()+word[1:]
+    if wordFirstUpper and wordFirstLower in entries:
+        print("We found two entries for **",word,"** as",wordFirstUpper,"and",wordFirstLower)
+        print("Which one do you mean?","[1] for",wordFirstUpper,"/ [2] for",wordFirstLower,":")
+        answer = input()
+        if int(answer) == 1:
+            return wordFirstUpper
+        else:
+            return wordFirstLower
+    
+
 word = input("Enter a word: ")
+
+
+    
+    
 print (findEntry(word))
 
 
